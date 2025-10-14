6fcc79e257ff9e6b4a541cd26ed38363669d76c9
Mario <mariocode616@gmail.com>
2025-10-14 12:52:08 -0500

    Actualiza footer: agrega enlace a MFDev con logo y ubicación

---
+++ src/app/pages/Inicio/Components/inicio-Footer/inicio-Footer.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'Inicio-footer',
+  imports: [],
+  templateUrl: './inicio-Footer.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InicioFooterComponent { }
--- src/app/pages/Inicio/Inicio/Inicio.component.ts
+++ src/app/pages/Inicio/Inicio/Inicio.component.ts
@@ -2,10 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { InicioCarruselComponent } from '../Inicio-Carrusel/Inicio-Carrusel.component';
 import { InicioGridComponent } from '../Inicio-Grid/Inicio-Grid.component';
 import { InicioNosotrosComponent } from '../Inicio-Nosotros/Inicio-Nosotros.component';
+import { InicioFooterComponent } from '../Components/inicio-Footer/inicio-Footer.component';
+
 
 @Component({
   selector: 'Inicio',
-  imports: [InicioCarruselComponent,InicioGridComponent,InicioNosotrosComponent],
+  imports: [InicioCarruselComponent,InicioGridComponent,InicioNosotrosComponent,InicioFooterComponent],
   templateUrl: './Inicio.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
